ff4c63af1cb25802feeaae38505e9634ee936487
ekra <ekranyambura@gmail.com>
2014-09-18 17:27:52 +0300

    added delete to the form

--- laravel/app/views/tasks/index.blade.php
+++ laravel/app/views/tasks/index.blade.php
@@ -9,9 +9,10 @@
 	<li>{{ $task->text }}</li>
 	<!-- {{  link_to_route('tasks.edit', 'Edit task')  }} -->
 <a class="btn btn-info" href="{{ URL::to('tasks/' . $task->id . '/edit') }}">Edit task</a>
-{{ Form::open(array('url' => 'tasks/' . $task->id, 'class' => 'pull-right')) }}
+
+{{ Form::open(['url' => 'tasks/' . $task->id, 'class' => 'pull-right']) }}
 					{{ Form::hidden('_method', 'DELETE') }}
-					{{ Form::submit('Delete this Task', array('class' => 'btn btn-warning')) }}
+					{{ Form::submit('Delete this Task', ['class' => 'btn btn-warning']) }}
 				{{ Form::close() }}
 	@endforeach
 
@@ -19,8 +20,13 @@
 
 <div>{{ link_to_route('tasks.create', 'Create task') }}</div>
 
-
-<div>{{ link_to_route('sessions.create', 'Log out') }}</div>
+@if(Auth::check())
+<div>
+{{Form::open(['action' => 'SessionsController@destroy', 'method' => 'DELETE'])}}
+{{Form::submit('Logout')}}
+{{Form::close()}}
+</div>
+@endif
 
 
 @stop
