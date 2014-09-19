a008eb99430c6321062d2702b46609a39a60b026
ekra <ekranyambura@gmail.com>
2014-09-19 14:51:41 +0300

    added design task

--- laravel/app/views/tasks/create.blade.php
+++ laravel/app/views/tasks/create.blade.php
@@ -3,15 +3,27 @@
 
 @section('content')
 
+<head>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<style>
+body
+ { padding-bottom:40px; padding-top:40px; }
+</style>
+</head>
+<body class="container">
+<div class="row">
+<div class="col-sm-8 col-sm-offset-2">
+
 {{ Form::open(array('route'=>'tasks.store')) }}
 
 <div class="form_group">
 
 {{Form::label('Add Task')}}
 
-{{ Form::text('text') }}
-{{Form::submit('Create task', ['class' => 'btn btn-success'])}}
+{{ Form::text('text' , null, ['class' => 'form-control']) }}
 
+{{Form::submit('Create task', ['class' => 'btn btn-success'])}}
+</div>
 {{Form::close()}}
 
 </div>
