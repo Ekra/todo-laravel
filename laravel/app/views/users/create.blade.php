2dc459311aee556cf02c33063c1ddcf5c59a1ac0
ekra <ekranyambura@gmail.com>
2014-09-19 14:54:13 +0300

    styled users form

--- laravel/app/views/users/create.blade.php
+++ laravel/app/views/users/create.blade.php
@@ -1,34 +1,45 @@
 @extends('layouts.default')
 
 @section('content')
-
+<head>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<style>
+body
+ { padding-bottom:40px; padding-top:40px; }
+</style>
+</head>
+<body class="container">
+<div class="row">
+<div class="col-sm-8 col-sm-offset-2">
+<div class="page-header">
 	<h1>Create New Users </h1>
+</div>
 
 	{{Form::open(['route' => 'users.store']) }}
-<div>
-
+<div class="form_group">
 	{{Form::label('username','username:')}}
-	{{Form::input('text','username')}}
+	{{Form::text('username', null ,[ 'class'=>'form-control'])}}
 	{{$errors->first('username')}}
 </div>
 
 
-<div>
-
+<div class="form_group">
 	{{Form::label('email','Email:')}}
-	{{Form::email('email')}}
+	{{Form::email('email',null, ['class' => 'form-control'])}}
 	{{$errors->first('email')}}
 </div>
 
-<div>
+<div class="form_group">
 
 	{{Form::label('password','password:')}}
-	{{Form::input('password','password')}}
+	{{Form::password('password', ['class' => 'form-control'])}}
 	{{$errors->first('password')}}
 </div>
 
-<div>{{Form::submit('Create user') }}
-	{{Form::close()}}
+<div class="form_group">
+	{{Form::submit('Create user',['class' => 'btn btn-success']) }}
+</div>
 
+	{{Form::close()}}
 @stop
 
