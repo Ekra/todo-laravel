31cc35fe43619203ef27a0ab1f59bac859453236
ekra <ekranyambura@gmail.com>
2014-09-19 14:52:33 +0300

    styled edit

--- laravel/app/views/tasks/edit.blade.php
+++ laravel/app/views/tasks/edit.blade.php
@@ -1,4 +1,22 @@
-<h1>Edit {{ $tasks->text }}</h1>
+
+@extends('layouts.default')
+
+@section('content')
+
+<head>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<style>
+body
+ { padding-bottom:40px; padding-top:40px; }
+</style>
+</head>
+<body class="container">
+<div class="row">
+<div class="col-sm-8 col-sm-offset-2">
+<div class="page-header">
+
+	<h1>Edit {{ $tasks->text }}</h1>
+</div>
 
 <!-- if there are creation errors, they will show here -->
 {{ HTML::ul($errors->all()) }}
